dcdd3dd3c7e80c5bcbcd43f77c8ac94c1594d25e
Wishmaster <wmts@arkadium.com>
2016-10-24 12:24:20 -0400

    add module configuration as apart of interface

--- src/InhabitModuleEvents.js
+++ src/InhabitModuleEvents.js
@@ -3,36 +3,51 @@
  * Created by WMTS on 10/24/2016.
  */
 function InhabitModuleEvents() {
-
+    this.interfaceMethod = function interfaceMethod() {
+        console.log("this is interface method would be implemented later through the core");
+    };
+    /**
+     * Configuration that will be passed to the analytics
+     * @type {{name: string, version: string}}
+     */
+    this.moduleConfiguration = {
+        "name":"ModuleName",
+        "version":"0.0.1"
+    }
 }
 InhabitModuleEvents.prototype =
 {
     /**
+     * Initializes module events, should be called first, before any interactions
+     * @param {{name: string, version: string}} moduleConfiguration
+     */
+    init:function (moduleConfiguration) {
+        this.moduleConfiguration = moduleConfiguration;
+    },
+    /**
      * Fire this event when interactive module loaded all required resources and ready to be displayed to user
      * this triggers hides general preloader.
      */
-    ready: undefined.interfaceMethod,
+    ready: this.interfaceMethod,
     /**
      * In case of any error happens inside interactive, fire this event, this will trigger sequence that will hide current
      * interactive and show next in the list
      */
-    error: undefined.interfaceMethod,
+    error: this.interfaceMethod,
     /**
      * Fire this event when user started his interaction with you module. This event should be triggered once per lifetime
      */
-    interactionStart: undefined.interfaceMethod,
+    interactionStart: this.interfaceMethod,
     /**
      * Trigger this event when user starts new sequence in your module.
      */
-    cycleStart: undefined.interfaceMethod,
+    cycleStart: this.interfaceMethod,
     /**
      * Trigger this event when users completes cycle and about to start new one
      */
-    cycleEnd: undefined.interfaceMethod,
+    cycleEnd: this.interfaceMethod
 
-    interfaceMethod: function interfaceMethod() {
-        console.log("this is interface method would be implemented later through the core");
-    }
+
 };
 
 module.exports = InhabitModuleEvents;
