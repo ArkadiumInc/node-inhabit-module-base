409ddeaeb34144e80625ee815b9580908ba1d1d6
Wishmaster <wmts@arkadium.com>
2016-10-24 13:13:24 -0400

    fix undefined

--- src/InhabitModuleEvents.js
+++ src/InhabitModuleEvents.js
@@ -3,9 +3,6 @@
  * Created by WMTS on 10/24/2016.
  */
 function InhabitModuleEvents() {
-    this.interfaceMethod = function interfaceMethod() {
-        console.log("this is interface method would be implemented later through the core");
-    };
     /**
      * Configuration that will be passed to the analytics
      * @type {{name: string, version: string}}
@@ -28,26 +25,29 @@ InhabitModuleEvents.prototype =
      * Fire this event when interactive module loaded all required resources and ready to be displayed to user
      * this triggers hides general preloader.
      */
-    ready: this.interfaceMethod,
+    ready: interfaceMethod,
     /**
      * In case of any error happens inside interactive, fire this event, this will trigger sequence that will hide current
      * interactive and show next in the list
      */
-    error: this.interfaceMethod,
+    error: interfaceMethod,
     /**
      * Fire this event when user started his interaction with you module. This event should be triggered once per lifetime
      */
-    interactionStart: this.interfaceMethod,
+    interactionStart: interfaceMethod,
     /**
      * Trigger this event when user starts new sequence in your module.
      */
-    cycleStart: this.interfaceMethod,
+    cycleStart: interfaceMethod,
     /**
      * Trigger this event when users completes cycle and about to start new one
      */
-    cycleEnd: this.interfaceMethod
+    cycleEnd: interfaceMethod
 
 
 };
+function interfaceMethod() {
+    console.log("this is interface method would be implemented later through the core");
+}
 
 module.exports = InhabitModuleEvents;
